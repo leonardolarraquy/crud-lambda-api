6b2b880678592a1ec5f0cf811f1c92108edcf0a4
leonardolarraquy <leonardo.larraquy@MacBook-Pro-de-Leonardo.local>
2024-08-16 14:51:21 -0300

    added logging to function

--- src/main/java/com/tmobs/aws/crud/lambda/consumer/SqsConsumer.java
+++ src/main/java/com/tmobs/aws/crud/lambda/consumer/SqsConsumer.java
@@ -1,12 +1,22 @@
 package com.tmobs.aws.crud.lambda.consumer;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -17,9 +27,44 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class SqsConsumer implements RequestHandler<Object, String>{
 
+	/*
+	public static void main(String[] args) {
+
+		String body ="{\"url\": \"https://www.la.clover.com/p/DJ90NDQHYP7CY\", \"country\": \"Argentina\"}";
+		body = body.replaceAll("\"", "");
+
+		// Eliminar las llaves externas
+		String input = body.substring(1, body.toString().length() - 1);
+
+		// Dividir la cadena por ", " para obtener cada par clave-valor
+		String[] pairs = input.split(", ");
+
+		// Crear un mapa para almacenar los pares clave-valor
+		Map<String, String> map = new HashMap<>();
+
+		// Iterar sobre los pares
+		for (String pair : pairs) {
+			// Dividir cada par por "="
+			String[] keyValue = pair.split(":", 2);
+			String key = keyValue[0].trim();
+			String value = keyValue[1].trim();
+			// Agregar al mapa
+			map.put(key, value);
+		}
+
+		System.out.println("mapa es: " +map);
+
+	}
+	 */
+
 	private void logContext(Context context) {
 		if(context.getClientContext() != null &&  context.getClientContext().getEnvironment() != null)
 			context.getLogger().log("logging call: " + context.getClientContext().getEnvironment().toString());
+
+		context.getLogger().log("Java Version: " + System.getProperty("java.version"));
+		context.getLogger().log("Java Runtime Version: " + System.getProperty("java.runtime.version"));
+		context.getLogger().log("Java Vendor: " + System.getProperty("java.vendor"));
+		context.getLogger().log("Java Home: " + System.getProperty("java.home"));
 
 		context.getLogger().log("logging call: " + context.getFunctionName());
 		context.getLogger().log("logging call: " + context.toString());
@@ -30,78 +75,97 @@ public class SqsConsumer implements RequestHandler<Object, String>{
 	@Override
 	public String handleRequest(Object event, Context context) {
 		this.logContext(context);		
-		context.getLogger().log("variable Event: " + event);
+		context.getLogger().log("variable Event: " + event + " class: " + event.getClass().getName()) ;
 
-		WebClient webClient = new WebClient(BrowserVersion.CHROME);
+		//		java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
+		//		java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
 
-		try {			
-			context.getLogger().log("Java Version: " + System.getProperty("java.version"));
-			context.getLogger().log("Java Runtime Version: " + System.getProperty("java.runtime.version"));
-			context.getLogger().log("Java Vendor: " + System.getProperty("java.vendor"));
-			context.getLogger().log("Java Home: " + System.getProperty("java.home"));
-						
-			java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
-			java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
+		LinkedHashMap mapa = (LinkedHashMap) event;
 
+		Iterator it = mapa.keySet().iterator();
+		while (it.hasNext()) {
+			String x    = it.next().toString();
 
-			// Eliminar las llaves externas
-			String input = event.toString().substring(1, event.toString().length() - 1);
+			context.getLogger().log("x: " + x);
 
-			// Dividir la cadena por ", " para obtener cada par clave-valor
-			String[] pairs = input.split(", ");
+			List list   = (List) mapa.get(x);
 
-			// Crear un mapa para almacenar los pares clave-valor
-			Map<String, String> map = new HashMap<>();
+			context.getLogger().log("list: " + list + " size: " + list.size());
 
-			// Iterar sobre los pares
-			for (String pair : pairs) {
-				// Dividir cada par por "="
-				String[] keyValue = pair.split("=", 2);
-				String key = keyValue[0].trim();
-				String value = keyValue[1].trim();
-				// Agregar al mapa
-				map.put(key, value);
+			Map obj     = (Map) list.get(0);
+
+			context.getLogger().log("obj: " + obj + " size: " + obj.size());
+
+			String body    = obj.get("body").toString();
+
+			context.getLogger().log("body: " + body + " body: " + body.length());
+
+			WebClient webClient = new WebClient(BrowserVersion.CHROME);
+
+			try {							
+
+				body = body.replaceAll("\"", "");
+
+				// Eliminar las llaves externas
+				String input = body.substring(1, body.toString().length() - 1);
+
+				// Dividir la cadena por ", " para obtener cada par clave-valor
+				String[] pairs = input.split(", ");
+
+				// Crear un mapa para almacenar los pares clave-valor
+				Map<String, String> map = new HashMap<>();
+
+				// Iterar sobre los pares
+				for (String pair : pairs) {
+					// Dividir cada par por "="
+					String[] keyValue = pair.split(":", 2);
+					String key = keyValue[0].trim();
+					String value = keyValue[1].trim();
+					// Agregar al mapa
+					map.put(key, value);
+				}
+
+				String url = (String) map.get("url").toString();
+
+				this.openUrlConnection(context, url);
+
+				//todo true
+				webClient.getOptions().setJavaScriptEnabled(true);
+				webClient.getOptions().setUseInsecureSSL(true);
+				webClient.getOptions().setRedirectEnabled(true);
+				webClient.getCookieManager().setCookiesEnabled(true);
+
+				//todo false			
+				webClient.getOptions().setCssEnabled(false);
+				webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+				webClient.getOptions().setThrowExceptionOnScriptError(false);
+				webClient.getOptions().setPrintContentOnFailingStatusCode(false);
+
+				//tiempos
+				webClient.getOptions().setTimeout(15000); //socket connection and data retrieval
+				webClient.waitForBackgroundJavaScriptStartingBefore(1000);
+				webClient.waitForBackgroundJavaScript(1000);
+
+				webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+
+				long now = System.currentTimeMillis();
+
+				context.getLogger().log("WEBCLIENT CONFIGURED ABOUT TO MAKE DE CALL ");
+
+				HtmlPage page = webClient.getPage(url);
+
+				int code    = page.getWebResponse().getStatusCode();
+				String resm = page.getWebResponse().getStatusMessage();
+
+				context.getLogger().log("URL already called: " + url + " HTTP CODE: " + code + " RESPONSE: " + resm + " PAGE TITLE: " + page.getTitleText() + " time: " + (System.currentTimeMillis() - now) + " ms.");
+
 			}
-
-			context.getLogger().log("URL to Get: " + map.get("url").toString());
-			
-			long now = System.currentTimeMillis();
-			
-			this.openUrlConnection(context, map.get("url").toString());
-
-			context.getLogger().log("URL already called using HttpURLConnection: " + (System.currentTimeMillis() - now) + " ms.");
-			
-			//todo true
-			webClient.getOptions().setJavaScriptEnabled(false);
-			webClient.getOptions().setUseInsecureSSL(true);
-			webClient.getOptions().setRedirectEnabled(true);
-			webClient.getCookieManager().setCookiesEnabled(true);
-			
-			//todo false			
-			webClient.getOptions().setCssEnabled(false);
-			webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-			webClient.getOptions().setPrintContentOnFailingStatusCode(false);
-			
-			//tiempos
-			webClient.getOptions().setTimeout(15000); //socket connection and data retrieval
-			webClient.waitForBackgroundJavaScriptStartingBefore(1000);
-			webClient.waitForBackgroundJavaScript(1000);
-
-			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-
-			context.getLogger().log("WEBCLIENT CONFIGURED ABOUT TO MAKE DE CALL ");
-
-			HtmlPage page = webClient.getPage((String) map.get("url").toString());
-
-			context.getLogger().log("URL already called: " + page.getTitleText());
-
-		}
-		catch(Exception e) {
-			context.getLogger().log("error parsing url: " + e.getMessage());
-		}
-		finally {
-			webClient.close();			
+			catch(Exception e) {
+				context.getLogger().log("error parsing url: " + e.toString());
+			}
+			finally {
+				webClient.close();			
+			}
 		}
 
 
@@ -109,7 +173,19 @@ public class SqsConsumer implements RequestHandler<Object, String>{
 
 	}
 
-	private void openUrlConnection(Context context, String urlString ) {
+	public static void main(String[] args) {
+
+		openUrlConnection(null, "https://amtae.co/qfrqx");
+
+	}
+
+
+	private static void  openUrlConnection(Context context, String urlString ) {		
+		//		context.getLogger().log("URL to Get: " + urlString);
+
+		long now = System.currentTimeMillis();
+
+
 		HttpURLConnection connection = null;
 
 		try {
@@ -118,17 +194,29 @@ public class SqsConsumer implements RequestHandler<Object, String>{
 
 			// Abrir conexión
 			connection = (HttpURLConnection) url.openConnection();
-
+			SSLContext sc = SSLContext.getInstance("SSL");  
+			sc.init(null, new TrustManager[]{new TrustAnyTrustManager()}, new java.security.SecureRandom());  
+			// Create all-trusting host name verifier
+			HostnameVerifier allHostsValid = new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			};
+			//           connection.setHostnameVerifier(allHostsValid);
 			// Configurar el método de solicitud (GET por defecto)
 			connection.setRequestMethod("GET");
 
 			// Establecer un tiempo de espera para la conexión
 			connection.setConnectTimeout(5000);
 			connection.setReadTimeout(5000);
+			connection.setFollowRedirects(true);
+			connection.setUseCaches(false);
+
+			connection.connect();
 
 			// Obtener el código de respuesta
 			int responseCode = connection.getResponseCode();
-			context.getLogger().log("Response Code by URL Connect: " + responseCode);
+			//			context.getLogger().log("Response Code by URL Connect: " + responseCode);
 
 			// Leer la respuesta si el código es 200 (OK)
 			if (responseCode == HttpURLConnection.HTTP_OK) {
@@ -144,10 +232,10 @@ public class SqsConsumer implements RequestHandler<Object, String>{
 				in.close();
 
 			} else {
-				context.getLogger().log("Failed to connect: " + responseCode);
+				//				context.getLogger().log("Failed to connect: " + responseCode);
 			}
 
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} 
 		finally {
@@ -155,5 +243,21 @@ public class SqsConsumer implements RequestHandler<Object, String>{
 				connection.disconnect();  // Asegurarse de desconectar la conexión
 			}
 		}
+
+		//		context.getLogger().log("URL already called using HttpURLConnection: " + urlString + " " + (System.currentTimeMillis() - now) + " ms.");
+
+	}
+}
+
+class TrustAnyTrustManager implements X509TrustManager {
+
+	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	}
+
+	public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	}
+
+	public X509Certificate[] getAcceptedIssuers() {
+		return null;
 	}
 }
